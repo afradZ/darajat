cc3fd426f135a70a72be91df0f59d1bf2bc9ab14
afradZ <afradzakaria@gmail.com>
2026-05-31 23:59:17 +0100

    issue resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_P5bhB1FCeDos@ep-floral-forest-abnxfdkd.eu-west-2.aws.neon.tech/neondb?sslmode=require',
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
